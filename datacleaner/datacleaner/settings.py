3d803d6a19afa11cab1bb95be7fc90091729c2d6
uriel sambiani <urielsambiani00@gmail.com>
2022-05-05 01:05:11 +0100

    adding the docker files and setting up the Mysql database and start of the model creation

--- datacleaner/datacleaner/settings.py
+++ datacleaner/datacleaner/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'datacleaner.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'data-app-db',
+        'USER' : 'root',
+        'PASSWORD': '',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
 
